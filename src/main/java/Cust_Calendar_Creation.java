fb171ebf412e307c683a1ebbc44453f729b99306
AWhitaker59 <alwhitaker6024@gmail.com>
2023-11-13 10:48:46 -0600

    Can now add reservations via the UI and properly store them in the database as well as display them to the Calendar upon creation. Also created a new modal in the HTML to show a 'Delete Reservation' option. Delete functionality to come.

--- src/main/java/Cust_Calendar_Creation.java
+++ src/main/java/Cust_Calendar_Creation.java
@@ -20,28 +20,123 @@ import org.json.JSONObject;
 public class Cust_Calendar_Creation extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+
     public Cust_Calendar_Creation() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+		response.setContentType("application/json;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		Connection connection = null;
+		String grabReservations = "Select * FROM Reservations"; //TODO-select by username
+		
+		try {
+			DBConnectionDelta.getDBConnection();
+			connection = DBConnectionDelta.connection;
+			
+			PreparedStatement preparedStatement = connection.prepareStatement(grabReservations);
+			//preparedStatement.setString(1, "username"); //TODO- get username as variable from JS
+			ResultSet resultSet = preparedStatement.executeQuery();
+			
+			JSONArray reservationsArray = new JSONArray();
+			
+			while (resultSet.next()) {
+				int id = resultSet.getInt("id");
+				String first_name = resultSet.getString("first_name");
+				String last_name = resultSet.getString("last_name");
+				String date = resultSet.getString("date");
+				String time = resultSet.getString("time");   //TODO
+				JSONObject reservation = new JSONObject();
+				reservation.put("id", id);
+				reservation.put("first_name", first_name);
+				reservation.put("last_name", last_name);
+				reservation.put("date", date);
+				reservation.put("time", time);
+				
+				reservationsArray.put(reservation);
+			}
+			JSONObject jsonResponse = new JSONObject();
+            jsonResponse.put("success", true);
+            jsonResponse.put("reservation", reservationsArray);
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+            out.print(jsonResponse.toString());
+            out.flush();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            System.err.println("Database error: " + e.getMessage());
+        } catch (JSONException e) { 
+            e.printStackTrace();
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            System.err.println("JSON error: " + e.getMessage());
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+	    response.setContentType("application/json;charset=UTF-8");
+	    PrintWriter out = response.getWriter();
 
+	    try {
+	        JSONObject requestData = new JSONObject(request.getReader().lines().reduce("", (accumulator, actual) -> accumulator + actual));
+	        String first_name = requestData.getString("first_name");
+	        String last_name = requestData.getString("last_name");
+	        String phone = requestData.getString("phone");
+	        String email = requestData.getString("email");
+	        String date = requestData.getString("date");
+	        String time = requestData.getString("time");
+
+	        Connection connection = null;
+	        String addReservation = "INSERT INTO Reservations (first_name, last_name, phone, email, date, time) VALUES (?, ?, ?, ?, ?, ?)";
+
+	        try {
+	            DBConnectionDelta.getDBConnection();
+	            connection = DBConnectionDelta.connection;
+
+	            PreparedStatement preparedStatement = connection.prepareStatement(addReservation, Statement.RETURN_GENERATED_KEYS);
+	            preparedStatement.setString(1, first_name);
+	            preparedStatement.setString(2, last_name);
+	            preparedStatement.setString(3, phone);
+	            preparedStatement.setString(4, email);
+	            preparedStatement.setString(5, date);
+	            preparedStatement.setString(6, time);
+
+	            int affectedRows = preparedStatement.executeUpdate();
+	            if (affectedRows == 0) {
+	                response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+	                System.err.println("Failed to insert reservation.");
+	                return;
+	            }
+
+	            ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
+	            if (generatedKeys.next()) {
+	                int id = generatedKeys.getInt(1);
+	                JSONObject jsonResponse = new JSONObject();
+	                jsonResponse.put("success", true);
+	                jsonResponse.put("id", id);
+	                out.println(jsonResponse.toString());
+	            } else {
+	                response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+	                System.err.println("Failed to get id.");
+	            }
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+	            System.err.println("Database error: " + e.getMessage());
+	        }
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+	        System.err.println("Error parsing JSON data: " + e.getMessage());
+	    }
+	}
 }
--- src/main/webapp/customer_cal_script.js
+++ src/main/webapp/customer_cal_script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
-  load();
-  initButtons();
+	initButtons();
+	load();
 });
 
 let nav = 0;
@@ -15,115 +15,236 @@ const customerEmail = document.getElementById('customerEmail');
 const modalBackDrop = document.getElementById('modalBackDrop');
 const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const reservationTimes = ['09:00 AM', '12:00 PM', '06:00 PM', '08:00 PM'];
+let data;
 
 function openReservationTimesModal(date) {
-  document.getElementById('selectedDate').innerText = date;
-  const timesContainer = document.getElementById('timesContainer');
-  timesContainer.innerHTML = ''; 
+	document.getElementById('selectedDate').innerText = date;
+	const timesContainer = document.getElementById('timesContainer');
+	timesContainer.innerHTML = ''; 
 	
-  for (const time of reservationTimes) {
-    const timeButton = document.createElement('button');
-    timeButton.textContent = time;
-    timeButton.classList.add('time-button');
-    timesContainer.appendChild(timeButton);
-  }
+	for (const time of reservationTimes) {
+		const timeButton = document.createElement('button');
+		timeButton.textContent = time;
+		timeButton.classList.add('time-button');
+		timesContainer.appendChild(timeButton);
+	}
 
-  const reservationTimesModal = document.getElementById('reservationTimesModal');
-  reservationTimesModal.style.display = 'block';
-  
-  document.getElementById('closeTimesButton').addEventListener('click', closeReservationTimesModal);
-  const timeButtons = document.querySelectorAll('.time-button');
-  timeButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-      const selectedTime = button.textContent;
-      openNewReservationModal(selectedTime, date);
-    });
+	const reservationTimesModal = document.getElementById('reservationTimesModal');
+	reservationTimesModal.style.display = 'block';
+	
+	document.getElementById('closeTimesButton').addEventListener('click', closeReservationTimesModal);
+	const timeButtons = document.querySelectorAll('.time-button');
+	timeButtons.forEach((button) => {
+		button.addEventListener('click', () => {
+			const selectedTime = button.textContent;
+			openNewReservationModal(selectedTime, date);
+		});
+	});
+}
+
+function openDeleteReservationModal(selectedDate) {
+  const deleteReservationModal = document.getElementById('deleteReservationModal');
+  deleteReservationModal.style.display = 'block';
+
+  document.getElementById('selectedDateDelete').innerText = selectedDate;
+
+  document.getElementById('deleteReservation').addEventListener('click', () => {
+    deleteReservation(selectedDate);
+  });
+
+  document.getElementById('goBack').addEventListener('click', () => {
+    closeDeleteReservationModal();
   });
 }
 
-function closeReservationTimesModal() {
-  const reservationTimesModal = document.getElementById('reservationTimesModal');
-  reservationTimesModal.style.display = 'none';
-  closeModal();
+
+function closeDeleteReservationModal() {
+  const deleteReservationModal = document.getElementById('deleteReservationModal');
+  deleteReservationModal.style.display = 'none';
+  modalBackDrop.style.display = 'none';
+  clicked = null;
 }
 
 function openModal(date) {
   clicked = date;
-  openReservationTimesModal(date);
+  const existingReservation = data.reservation.find(reservation => reservation.date === date);
+
+  if (existingReservation) {
+    openDeleteReservationModal(date);
+  } else {
+    openReservationTimesModal(date);
+  }
+
   backDrop.style.display = 'block';
 }
 
 function openNewReservationModal(selectedTime, selectedDate) {
-  const newReservationModal = document.getElementById('newReservationModal');
-  newReservationModal.style.display = 'block';
-  document.getElementById('selectedDateReservation').innerText = selectedDate;
-  document.getElementById('selectedTime').innerText = selectedTime;
+	const newReservationModal = document.getElementById('newReservationModal');
+	newReservationModal.style.display = 'block';
+	document.getElementById('selectedDateReservation').innerText = selectedDate;
+	document.getElementById('selectedTime').innerText = selectedTime;
+}
+
+function fetchReservations() {
+	fetch('CustomerCalendar') 
+		.then(response => {
+			if (!response.ok) {
+				throw new Error(`HTTP error! Status: ${response.status}`);
+			}
+			return response.json();
+		})
+		.then(reservationsData => {
+			data = reservationsData;
+			displayReservations(reservationsData);
+		})
+		.catch(error => {
+			console.error('Error fetching reservations:', error);
+		});
+}
+
+function displayReservations(reservationsData) {
+	const days = document.querySelectorAll('.day');
+	days.forEach(day => {
+		const existingReservations = day.getElementsByClassName('reservation-info');
+		while (existingReservations.length > 0) {
+			existingReservations[0].parentNode.removeChild(existingReservations[0]);
+		}
+	});
+
+	reservationsData.reservation.forEach(reservation => {
+		const dateParts = reservation.date.split('/');
+		const reservationDate = new Date(dateParts[2], dateParts[0] - 1, dateParts[1]);
+
+		days.forEach(day => {
+			const dayNumber = parseInt(day.innerText);
+			const currentMonth = reservationDate.getMonth() + 1; 
+			const currentYear = reservationDate.getFullYear();
+
+			if (
+				!isNaN(dayNumber) &&
+				dayNumber === reservationDate.getDate() &&
+				currentMonth === month + 1 && 
+				currentYear === year
+			) {
+				const reservationInfo = document.createElement('div');
+				reservationInfo.classList.add('reservation-info');
+				reservationInfo.innerHTML = `<p>${reservation.first_name} ${reservation.last_name}</p><p>${reservation.time}</p>`;
+				reservationInfo.classList.add('small-text');
+				day.appendChild(reservationInfo);
+			}
+		});
+	});
 }
 
 function load() {
-  const dt = new Date();
+	const dt = new Date();
 
-  if (nav !== 0) {
-    dt.setMonth(new Date().getMonth() + nav);
-  }
+	if (nav !== 0) {
+		dt.setMonth(new Date().getMonth() + nav);
+	}
 
-  const day = dt.getDate();
-  const month = dt.getMonth();
-  const year = dt.getFullYear();
+	month = dt.getMonth(); 
+	year = dt.getFullYear();
 
-  const firstDayOfMonth = new Date(year, month, 1);
-  const daysInMonth = new Date(year, month + 1, 0).getDate();
-  
-  const dateString = firstDayOfMonth.toLocaleDateString('en-us', {
-    weekday: 'long',
-    year: 'numeric',
-    month: 'numeric',
-    day: 'numeric',
-  });
-  const paddingDays = weekdays.indexOf(dateString.split(', ')[0]);
+	const day = dt.getDate();
+	const firstDayOfMonth = new Date(year, month, 1);
+	const daysInMonth = new Date(year, month + 1, 0).getDate();
+	
+	const dateString = firstDayOfMonth.toLocaleDateString('en-us', {
+		weekday: 'long',
+		year: 'numeric',
+		month: 'numeric',
+		day: 'numeric',
+	});
+	const paddingDays = weekdays.indexOf(dateString.split(', ')[0]);
 
-  document.getElementById('monthDisplay').innerText = 
-    `${dt.toLocaleDateString('en-us', { month: 'long' })} ${year}`;
+	document.getElementById('monthDisplay').innerText = 
+		`${dt.toLocaleDateString('en-us', { month: 'long' })} ${year}`;
 
-  calendar.innerHTML = '';
+	calendar.innerHTML = '';
 
-  for(let i = 1; i <= paddingDays + daysInMonth; i++) {
-    const daySquare = document.createElement('div');
-    daySquare.classList.add('day');
+	for(let i = 1; i <= paddingDays + daysInMonth; i++) {
+		const daySquare = document.createElement('div');
+		daySquare.classList.add('day');
 
-    const dayString = `${month + 1}/${i - paddingDays}/${year}`;
+		const dayString = `${month + 1}/${i - paddingDays}/${year}`;
 
-    if (i > paddingDays) {
-      daySquare.innerText = i - paddingDays;
-      
-      if (i - paddingDays === day && nav === 0) {
-        daySquare.id = 'currentDay';
-      }
+		if (i > paddingDays) {
+			daySquare.innerText = i - paddingDays;
+			
+			if (i - paddingDays === day && nav === 0) {
+				daySquare.id = 'currentDay';
+			}
 
+			daySquare.addEventListener('click', () => openModal(dayString));
+		} else {
+			daySquare.classList.add('padding');
+		}
 
-      daySquare.addEventListener('click', () => openModal(dayString));
-    } else {
-      daySquare.classList.add('padding');
-    }
-
-    calendar.appendChild(daySquare);    
-  }
+		calendar.appendChild(daySquare);    
+	}
+	fetchReservations();
 }
 
-function closeModal() {
-  const newReservationModal = document.getElementById('newReservationModal');
-  newReservationModal.style.display = 'none';
-  modalBackDrop.style.display = 'none';	
-  customerFirst.value = '';
-  customerLast.value = '';
-  customerPhone.value = '';
-  customerEmail.value = '';
-  clicked = null;
-  load();
+function closeReservationTimesModal() {
+	const reservationTimesModal = document.getElementById('reservationTimesModal');
+	reservationTimesModal.style.display = 'none';
+	modalBackDrop.style.display = 'none';	
+	customerFirst.value = '';
+	customerLast.value = '';
+	customerPhone.value = '';
+	customerEmail.value = '';
+	clicked = null;
+}
+
+function closeNewReservationModal(){
+	const newReservationModal = document.getElementById('newReservationModal');
+	newReservationModal.style.display = 'none';
+	customerFirst.value = '';
+	customerLast.value = '';
+	customerPhone.value = '';
+	customerEmail.value = '';
 }
 
 function saveReservation() {
+	const selectedDate = document.getElementById('selectedDate').innerText;
+	const selectedTime = document.getElementById('selectedTime').innerText;
+	const first_name = document.getElementById('customerFirst').value;
+	const last_name = document.getElementById('customerLast').value;
+	const phone = document.getElementById('customerPhone').value;
+	const email = document.getElementById('customerEmail').value;
 
+	const reservationData = {
+		first_name: first_name,
+		last_name: last_name,
+		phone: phone,
+		email: email,
+		date: selectedDate,
+		time: selectedTime
+	};
+
+	fetch('CustomerCalendar', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(reservationData),
+	})
+	.then(response => {
+		if (!response.ok) {
+			throw new Error(`Error:${response.status}`);
+		}
+		return response.json();
+	})
+	.then(responseData => {
+		console.log('Success!:', responseData);
+		closeReservationTimesModal();
+		closeNewReservationModal();
+		fetchReservations();
+	})
+	.catch(error => {
+		console.error('Error:', error);
+	});
 }
 
 function deleteReservation() {
@@ -131,15 +252,17 @@ function deleteReservation() {
 }
 
 function initButtons() {
-  document.getElementById('nextButton').addEventListener('click', () => {
-    nav++;
-    load();
-  });
+	document.getElementById('nextButton').addEventListener('click', () => {
+		nav++;
+		load();
+		fetchReservations();
+	});
 
-  document.getElementById('backButton').addEventListener('click', () => {
-    nav--;
-    load();
-  });
-  document.getElementById('saveButton').addEventListener('click', saveReservation);
-  document.getElementById('cancelButton').addEventListener('click', closeModal);
+	document.getElementById('backButton').addEventListener('click', () => {
+		nav--;
+		load();
+		fetchReservations();
+	});
+	document.getElementById('saveButton').addEventListener('click', saveReservation);
+	document.getElementById('cancelButton').addEventListener('click', closeNewReservationModal);
 }
