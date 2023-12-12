b9c565ab824f765768ff6dc73edb267c6a30b024
AWhitaker59 <alwhitaker6024@gmail.com>
2023-12-11 23:04:55 -0600

    Error Checking
    Ben added error checking for the login page.

--- src/main/java/Login.java
+++ src/main/java/Login.java
@@ -21,10 +21,13 @@ public class Login extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html");
+        String errorMessage = "Default";
         PrintWriter out = response.getWriter();
         // Get the username and password from the form
         String username = request.getParameter("username");
         String password = request.getParameter("password");
+        // Get the button pressed
+        String buttonPressed = request.getParameter("buttonClicked");
         // Create variables for the connection and the SQL Statement
         Connection connection = null;
         PreparedStatement preparedStatement = null;
@@ -32,27 +35,50 @@ public class Login extends HttpServlet {
         try {
             DBConnectionDelta.getDBConnection();
             connection = DBConnectionDelta.connection;
-            // Create the select statement
-            String selectSQL = "SELECT * FROM userAccounts WHERE USERNAME LIKE ?";
-            preparedStatement = connection.prepareStatement(selectSQL);
-            preparedStatement.setString(1, username);
-            // Store the results of the query
-            ResultSet result = preparedStatement.executeQuery();
-
-            // Check if an account was found
-            if (!result.next()) {
-                out.println("<html>No user found with this username</html>");
-                return;
-            }
-
-            // If an account was found, retrieve the password from the DB
-            String userPassword = result.getString("PASSWORD").trim();
-            // Check if the input password matches the password in the DB
-            if (userPassword.equals(password)) {
-                out.println("<html>Login success!</html>");
-                response.sendRedirect("Customer_Calendar.html");
+	        // Create the select statement
+	        String selectSQL = "SELECT * FROM userAccounts WHERE USERNAME LIKE ?";
+	        preparedStatement = connection.prepareStatement(selectSQL);
+	        preparedStatement.setString(1, username);
+	        // Store the results of the query
+	        ResultSet result = preparedStatement.executeQuery();
+	        // Check which button was pressed
+	        if (buttonPressed.equals("login")) {
+	            // Check if an account was found
+	            if (!result.next()) {
+	            	errorMessage = "No account exists with the given username.";
+	            	response.sendRedirect("Error?message=" + errorMessage);
+	            	return;
+	            }
+	
+	            // If an account was found, retrieve the password from the DB
+	            String userPassword = result.getString("PASSWORD").trim();
+	            // Check if the input password matches the password in the DB
+	            if (userPassword.equals(password)) {
+	                response.sendRedirect("Customer_Calendar.html");
+	            } else {
+	            	errorMessage = "Incorrect password, login failed.";
+	            	response.sendRedirect("Error?message=" + errorMessage);
+	                return;
+	            }
+	            
             } else {
-                out.println("<html>Incorrect password, login failed.</html>");
+            	// Check if an account with that username already exists
+            	if (result.next()) {
+            		errorMessage = "An account with that username already exists.";
+            		response.sendRedirect("Error?message=" + errorMessage);
+            		return;
+            	}
+            	// Create the insert statement
+            	String createSQL = "INSERT INTO userAccounts (USERNAME, PASSWORD) VALUES (?, ?)";
+            	preparedStatement = connection.prepareStatement(createSQL);
+            	preparedStatement.setString(1, username);
+            	preparedStatement.setString(2, password);
+            	// Execute the insert
+            	preparedStatement.executeUpdate();
+            	// Display success
+            	errorMessage = "Account successfully created!";
+            	response.sendRedirect("Error?message=" + errorMessage);
+            	return;
             }
 
         } catch (SQLException se) {
